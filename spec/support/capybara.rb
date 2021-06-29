e14c59bc7da58d9e5599938456ccff37786cf11b
me3eh <matt30002000@gmail.com>
2021-06-29 23:40:21 +0200

    Pushed to have

--- spec/support/capybara.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'capybara/rspec'
-
-Capybara.configure do |config|
-  config.app = SlackRubyBotServer::Api::Middleware.instance
-  config.server = :webrick
-  config.server_port = 9293
-end
