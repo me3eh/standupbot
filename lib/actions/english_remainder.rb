7ebe52caa97e1f7d9ea106e6bf327fff6fca69ec
me3eh <matt30002000@gmail.com>
2021-08-05 09:58:02 +0200

    Change time to match polish time

--- lib/actions/english_remainder.rb
+++ lib/actions/english_remainder.rb
@@ -3,7 +3,7 @@ instance = SlackRubyBotServer::Service.instance
 
 instance.every :hour do
   today_date_and_time = DateTime.now
-  if today_date_and_time.strftime('%A').eql?('Wednesday') && today_date_and_time.hour.eql?(11)
+  if today_date_and_time.strftime('%A').eql?('Wednesday') && today_date_and_time.hour.eql?(9)
     slack_client = $everything_needed.get_slack_client(team_id: 'T0A5H5F5M')
     # TODO change for team_id: T0A5H5F5M channel_id:CUMV4JP27
     # for test environment T01PQKG5X19/'C01PWJCSCKW'
@@ -16,7 +16,7 @@ end
 
 instance.every :minute do
   today_date_and_time = DateTime.now
-  if today_date_and_time.strftime('%A').eql?('Thursday') && today_date_and_time.hour.eql?(10) &&
+  if today_date_and_time.strftime('%A').eql?('Thursday') && today_date_and_time.hour.eql?(8) &&
     today_date_and_time.hour.eql?(59)
     slack_client = $everything_needed.get_slack_client(team_id: 'T0A5H5F5M')
     # TODO change for team_id: T0A5H5F5M channel_id:CUMV4JP27
