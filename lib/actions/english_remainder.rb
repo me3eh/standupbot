0cfbcb0332cfde25712a7b916c2a1925a60477c7
me3eh <matt30002000@gmail.com>
2021-09-15 00:45:14 +0200

    Changed time for heroku

--- lib/actions/english_remainder.rb
+++ lib/actions/english_remainder.rb
@@ -4,7 +4,7 @@ instance = SlackRubyBotServer::Service.instance
 instance.every :minute do
   today_date_and_time = DateTime.now
   if  today_date_and_time.strftime('%A').eql?('Thursday') &&
-    today_date_and_time.hour.eql?(9) &&
+    today_date_and_time.hour.eql?(8) &&
     today_date_and_time.minute > 58
     slack_client = $everything_needed.get_slack_client(team_id: 'T0A5H5F5M')
     # TODO change for team_id: T0A5H5F5M channel_id:CUMV4JP27
