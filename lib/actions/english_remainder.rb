5dec8bebadf96e18e1e846df440d63e123e64d2e
me3eh <matt30002000@gmail.com>
2021-09-15 00:40:34 +0200

    Changed values for english reminder

--- lib/actions/english_remainder.rb
+++ lib/actions/english_remainder.rb
@@ -1,29 +1,28 @@
 require 'date'
 instance = SlackRubyBotServer::Service.instance
 
-instance.every :hour do
-  today_date_and_time = DateTime.now
-  if today_date_and_time.strftime('%A').eql?('Wednesday') && today_date_and_time.hour.eql?(9)
-    slack_client = $everything_needed.get_slack_client(team_id: 'T0A5H5F5M')
-    # TODO change for team_id: T0A5H5F5M channel_id:CUMV4JP27
-    # for test environment T01PQKG5X19/'C01PWJCSCKW'
-    slack_client.chat_postMessage(
-      channel: 'CUMV4JP27',
-      text: "Przypominajka o jutrzejszym angielskim, by żarek nie musiał. Reakcja, jak idziesz"
-    )
-  end
-end
-
 instance.every :minute do
   today_date_and_time = DateTime.now
-  if today_date_and_time.strftime('%A').eql?('Thursday') && today_date_and_time.hour.eql?(8) &&
-    today_date_and_time.minute.eql?(59)
+  if  today_date_and_time.strftime('%A').eql?('Thursday') &&
+    today_date_and_time.hour.eql?(9) &&
+    today_date_and_time.minute > 58
     slack_client = $everything_needed.get_slack_client(team_id: 'T0A5H5F5M')
     # TODO change for team_id: T0A5H5F5M channel_id:CUMV4JP27
     # for test environment T01PQKG5X19/'C01PWJCSCKW'
     slack_client.chat_postMessage(
       channel: 'CUMV4JP27',
       text: "Angielski :slow_parrot:"
+    )
+  end
+  if  today_date_and_time.strftime('%A').eql?('Wednesday') &&
+    today_date_and_time.hour.eql?(9) &&
+    today_date_and_time.minute == 30
+    slack_client = $everything_needed.get_slack_client(team_id: 'T0A5H5F5M')
+    # TODO change for team_id: T0A5H5F5M channel_id:CUMV4JP27
+    # for test environment T01PQKG5X19/'C01PWJCSCKW'
+    slack_client.chat_postMessage(
+      channel: 'CUMV4JP27',
+      text: "Przypominajka o jutrzejszym angielskim, by żarek nie musiał. Reakcja, jak idziesz"
     )
   end
 end
